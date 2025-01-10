4a528efb67362ed4a9d7456ef331eef81dbf998a
Mwesigye Linda <lindamwesigye2@gmail.com>
2025-01-10 14:21:16 +0200

    Dashboard begin

---
+++ react/rest-app/src/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='w-full items-center justify-center bg-white'> 
+        <div className='flex'></div>
+    </div>
+  )
+}
+
+export default Dashboard
