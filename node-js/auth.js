78c0b3dc34a280c7ac0a47a434a9c66a740599dc
Mwesigye Linda <lindamwesigye2@gmail.com>
2025-01-16 17:03:27 +0200

    detokenization remove

--- node-js/auth.js
+++ node-js/auth.js
@@ -81,22 +81,6 @@ app.post("/login", async(req, res, next) =>{
       console.error("Error occurred:", err);
     });
 
-// app.get("/accessResource", (req, res) =>{
-//     const token = req.headers.authorization.split(' ')[1];
-//     if(!token){
-//         res.status(200).json({
-//             success: false,
-//             message: "Error! Token was not provided"
-//         });
-//     }
-//     const decodedToken = jwt.verify(token, "secretkeyappearshere");
-//     res.status(200).json({
-//         success: true,
-//         data: {
-//             userId: decodedToken.userId,
-//             email: decodedToken.email
-//         },
-//     });
-// })                    
+                 
 
                   
